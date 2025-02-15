2d9c4c1c3230df857cc9d91b75dc59ab361ea31b
nhanquach <qtrongnhan@gmail.com>
2025-02-16 00:22:33 +0700

    chore: setting up daisyui

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       manifest: {
         name: "TabSync - Your Tabs Accross Devices",
